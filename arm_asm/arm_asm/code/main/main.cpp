ac9470e4f794324ace648715b1dfa9123cad7e9c
Omar Tahir <otahir007@gmail.com>
2017-02-23 10:28:09 +0000

    Removed old commented out code from main.cpp

--- arm_asm/arm_asm/code/main/main.cpp
+++ arm_asm/arm_asm/code/main/main.cpp
@@ -5,30 +5,6 @@
 */
 
 #include <stdint.h>
-
-//#include "../sim/header/computer.h"
-
-//#include "../io/io.h"
-
 #include "../gui/header/app.h"
 
 wxIMPLEMENT_APP(App);
-
-// Entry point
-/*
-int main(int argc, char* argv[])
-{
-	std::vector<std::vector<std::string>> data;
-	Read("colours.txt", data);
-
-	std::vector<uint32_t> code;
-	Parse(data, code);
-
-	Computer<uint16_t> computer;
-	computer.Read(code);
-	computer.Run();
-
-	std::cin.ignore();
-	return 0;
-}
-*/
